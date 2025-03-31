8a7a643252695ed854810ac824d6fc170b8d4e9f
NokuManhango <noku789manhango@gmail.com>
2025-03-31 11:49:10 +0100

    Testing classes

--- src/main/java/edu/tus/employee/EmployeeApplication.java
+++ src/main/java/edu/tus/employee/EmployeeApplication.java
@@ -2,8 +2,10 @@ package edu.tus.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "edu.tus.employee.errors")  // Adjust the package to where your ErrorValidation class is
 public class EmployeeApplication {
 
 	public static void main(String[] args) {
